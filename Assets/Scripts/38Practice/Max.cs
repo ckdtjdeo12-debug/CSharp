9f61c259d46c31a93a55ff340c95b83c6ff5c50d
DESKTOP-5V3FRMH\M <ckdtjdeo12@gmail.com>
2025-09-24 15:15:21 +0900

    1.  practic클래스 예제

--- Assets/Scripts/38Practice/Max.cs
+++ Assets/Scripts/38Practice/Max.cs
@@ -5,6 +5,7 @@ namespace Practic
 {
     public class Max : MonoBehaviour
     {
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
